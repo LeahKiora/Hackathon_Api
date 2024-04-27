f66c975df287f9088ecd43cac11326017b5028c5
LeahKiora <161048061+LeahKiora@users.noreply.github.com>
2024-04-27 13:52:47 +0000

    ApiBuilding Hackathon

--- django_project/views.py
+++ django_project/views.py
@@ -1,3 +1,4 @@
+
 import requests
 from django.shortcuts import render 
 
@@ -16,5 +17,22 @@ def index(request):
   response2 = requests.get('https://freetestapi.com/api/v1/students') # Use this API
   data2 = response2.json()
   name = data2[0]['name']
+
+  # response4 = requests.get('https://api.punkapi.com/v2/beers')
+  # data4 = response4.json()
+  # beer = data4[0]['text']
+
+  response5 = requests.get('http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline')
+  data5 = response5.json()
+  product = data5[0]['name']
+
+  response6 = requests.get('https://v2.jokeapi.dev/joke/Any?safe-mode')
+  data6 = response6.json()
+  joke = data6['setup']
   
-  return render(request, 'templates/index.html', {'fact': fact, 'dog': dog,  'name': name})
+  #i dont know why my code is not executing
+  # response7 = requests.get('https://api.fisenko.net/v1/quotes/en?query=string&offset=0&limit=')
+  # data7 = response7.json()
+  # quote = data7[1]['quote']
+
+  return render(request, 'templates/index.html', {'fact': fact, 'dog': dog,  'name': name, 'product': product, 'joke': joke})
